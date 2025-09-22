14aecc3f444df879f6e399bda413dc583d6451c5
yinchunyuan <kjknuhb@outlook.com>
2025-09-22 16:40:43 +0800

    增加了一些内容

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
+
+
+
+
+
+
+
+
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        format: 'directory'
+    }
+
+// 不采用开发工具栏
+    devToolbar: {
+        enabled: false
+    }    
+});
